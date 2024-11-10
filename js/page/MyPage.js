6b9b0eadbaa8773144daa1df86c3c9dbe74c6be7
吴程亮 <924507542@qq.com>
2024-11-10 23:46:20 +0800

    feat(我的): 我的页面

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -1,31 +1,157 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import NavigationBar from 'react-native-navbar-plus';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import NavigationUtil from '../navigator/NavigationUtil';
 import {connect} from 'react-redux';
-import actions from '../action';
-import {Button} from 'react-native';
+import {onThemeChange} from '../action/theme';
+import {MORE_MENU} from '../common/MORE_MENU';
+import GlobalStyles from '../res/styles/GlobalStyles';
+import ViewUtil from '../util/ViewUtil';
+
 class MyPage extends Component {
+  onClick(menu) {
+    let RouteName,
+      params = {};
+    switch (menu) {
+      case MORE_MENU.Tutorial:
+        RouteName = 'WebViewPage';
+        params.title = '教程';
+        params.url = 'https://coding.m.imooc.com/classindex.html?cid=89';
+        break;
+      case MORE_MENU.About:
+        RouteName = 'AboutPage';
+        break;
+      case MORE_MENU.About_Author:
+        RouteName = 'AboutMePage';
+        break;
+    }
+    if (RouteName) {
+      NavigationUtil.goPage(params, RouteName);
+    }
+    if (RouteName) {
+      NavigationUtil.goPage(params, RouteName);
+    }
+  }
+  getItem(menu) {
+    const {theme: {themeColor} = {}} = this.props;
+    return ViewUtil.getMenuItem(() => this.onClick(menu), menu, themeColor);
+  }
+
   render() {
-    const {onThemeChange} = this.props;
+    const {theme: {themeColor} = {}} = this.props;
+    let statusBar = {
+      backgroundColor: themeColor,
+      barStyle: 'light-content',
+    };
+    let navigationBar = (
+      <NavigationBar
+        title={'我的'}
+        statusBar={statusBar}
+        style={{backgroundColor: themeColor}}
+      />
+    );
     return (
-      <View style={styles.container}>
-        <Text>最热</Text>
-        <Button
-          title="改变主题"
-          onPress={() => {
-            onThemeChange({themeColor: 'yellow'});
-          }}></Button>
+      <View style={GlobalStyles.root_container}>
+        {navigationBar}
+        <ScrollView>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.onClick(MORE_MENU.About)}>
+            <View style={styles.about_left}>
+              <Ionicons
+                name={MORE_MENU.About.icon}
+                size={40}
+                style={{
+                  marginRight: 10,
+                  color: themeColor,
+                }}
+              />
+              <Text>GitHub Popular</Text>
+            </View>
+            <Ionicons
+              name={'chevron-forward-outline'}
+              size={16}
+              style={{
+                marginRight: 10,
+                alignSelf: 'center',
+                color: themeColor,
+              }}
+            />
+          </TouchableOpacity>
+          <View style={GlobalStyles.line} />
+          {this.getItem(MORE_MENU.Tutorial)}
+          {/*趋势管理*/}
+          <Text style={styles.groupTitle}>趋势管理</Text>
+          {/*自定义语言*/}
+          {this.getItem(MORE_MENU.Custom_Language)}
+          {/*语言排序*/}
+          <View style={GlobalStyles.line} />
+          {this.getItem(MORE_MENU.Sort_Language)}
+
+          {/*最热管理*/}
+          <Text style={styles.groupTitle}>最热管理</Text>
+          {/*自定义标签*/}
+          {this.getItem(MORE_MENU.Custom_Key)}
+          {/*标签排序*/}
+          <View style={GlobalStyles.line} />
+          {this.getItem(MORE_MENU.Sort_Key)}
+          {/*标签移除*/}
+          <View style={GlobalStyles.line} />
+          {this.getItem(MORE_MENU.Remove_Key)}
+
+          {/*设置*/}
+          <Text style={styles.groupTitle}>设置</Text>
+          {/*自定义主题*/}
+          {this.getItem(MORE_MENU.Custom_Theme)}
+          {/*关于作者*/}
+          <View style={GlobalStyles.line} />
+          {this.getItem(MORE_MENU.About_Author)}
+          <View style={GlobalStyles.line} />
+          {/*反馈*/}
+          {this.getItem(MORE_MENU.Feedback)}
+        </ScrollView>
       </View>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  theme: state.theme.theme,
+});
 //将dispatch映射给onThemeChange，然后注入到组件的props中
 const mapDispatchToProps = dispatch => ({
-  onThemeChange: theme => dispatch(actions.onThemeChange(theme)),
+  onThemeChange: theme => dispatch(onThemeChange(theme)),
 });
-//包装 component，注入 dispatch到PopularTab
-export default connect(null, mapDispatchToProps)(MyPage);
+export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 30,
+  },
+  about_left: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  item: {
+    backgroundColor: 'white',
+    padding: 10,
+    height: 90,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  groupTitle: {
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 5,
+    fontSize: 12,
+    color: 'gray',
   },
 });
